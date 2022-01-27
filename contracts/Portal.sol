6ea13e5e31163d5f54fa7717845ccd9b7cfc1ef5
LiquidityHunter <connor@enso.finance>
2022-01-28 00:03:47 +0100

    commit prior to moving to yul

--- contracts/Portal.sol
+++ contracts/Portal.sol
@@ -1,14 +1,6 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-
-
-interface IERC20 {
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-}
-
 import "./VM.sol";
 
 library PortalErrors {
@@ -65,28 +57,3 @@ contract Portal {
         emit Added(_caller, msg.sender);
     }
 }
-
-
-
-/*
-    Inside of the weiroll contract, we check first call and only first call
-    to see if there is an approval contract associated with that user - need a 
-    storage mapping inside of weiroll like a registry.
-    Weiroll on first call, should also verify passed in token on first call
-    that it is approved on the approval contract, and for the amount.
-    Does not need to verify any future calls.
-
-    1. approvalFactory
-    2. approval contract
-    3. 
-    4. 
-
-
-
-1. Approve address of "ApprovalContract" (get address via create2 https://docs.ethers.io/v5/api/utils/address/#utils-getCreate2Address)
-2. Execute weiroll method
-    2.1 compute address of "ApprovalContract" with tx.origin --> if not contract address deploy a new one
-    2.2 transferFrom approvalContract (msg.sender == weiroll && tx.origin == owner)
-    2.3 do something with funds inside weiroll
-    2.4 transfer all funds to tx.origin
-*/
