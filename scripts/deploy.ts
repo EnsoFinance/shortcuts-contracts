0f0124b5efa2bbfd276fecb0042a517531449000
Ben Wolf <benjamineliaswolf@gmail.com>
2022-05-18 18:06:28 +0200

    Update yul branch to work with current version of ensonet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,44 @@
 const hre = require("hardhat");
+const fs = require("fs");
+
+const DEPLOYMENTS_JSON = require("../deployments.json");
+const deployments: { [key: string]: { [key: string]: string } } =
+  DEPLOYMENTS_JSON;
+let contracts: { [key: string]: string } = {};
+let network: string;
+
+// If true it will deploy contract regardless of whether there is an address currently on the network
+let overwrite = false;
+
+if (process.env.HARDHAT_NETWORK) {
+  network = process.env.HARDHAT_NETWORK;
+  //ts-ignore
+  if (deployments[network]) contracts = deployments[network];
+  if (network === "localhost") overwrite = true; // Overwrite on localhost
+  if (network === "mainnet") overwrite = false; // Don't overwrite on mainnet
+}
+
+const registerDeployment = (contractTitle: string, address: string) => {
+  contracts[contractTitle] = address;
+  console.log(`Deployed ${contractTitle}: ${address} 🚀`);
+  const data = JSON.stringify(
+    { ...deployments, [network]: contracts },
+    null,
+    2
+  );
+  fs.writeFileSync("./deployments.json", data);
+};
 
 async function main() {
   const EXECUTOR = await hre.ethers.getContractFactory("Executor");
   const executor = await EXECUTOR.deploy();
 
-  const TESTABLE_EXECUTOR = await hre.ethers.getContractFactory(
-    "TestableExecutor"
-  );
-  const testableExecutor = await TESTABLE_EXECUTOR.deploy(executor.address);
-
-  console.log(
-    "Testable weiroll executor deployed to:",
-    testableExecutor.address
-  );
+  const contractName = "TestableExecutor";
+  if (overwrite || !contracts[contractName]) {
+    const TESTABLE_EXECUTOR = await hre.ethers.getContractFactory(contractName);
+    const testableExecutor = await TESTABLE_EXECUTOR.deploy(executor.address);
+    registerDeployment(contractName, testableExecutor.address);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
