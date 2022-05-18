9f524a8a7b8428f3424f91c296234c9b281be930
Ben Wolf <benjamineliaswolf@gmail.com>
2022-05-18 17:38:12 +0200

    Modify deployments script to work with current version of ensonet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,44 @@
 const hre = require("hardhat");
+const fs = require("fs");
+
+const DEPLOYMENTS_JSON = require("../deployments.json");
+const deployments: { [key: string]: { [key: string]: string } } =
+  DEPLOYMENTS_JSON;
+let contracts: { [key: string]: string } = {};
+let network: string;
+
+// If true it will deploy contract regardless of whether there is an address currently on the network
+let overwrite = false;
+
+if (process.env.HARDHAT_NETWORK) {
+  network = process.env.HARDHAT_NETWORK;
+  //ts-ignore
+  if (deployments[network]) contracts = deployments[network];
+  if (network === "localhost") overwrite = true; // Overwrite on localhost
+  if (network === "mainnet") overwrite = false; // Don't overwrite on mainnet
+}
+
+const registerDeployment = (contractTitle: string, address: string) => {
+  contracts[contractTitle] = address;
+  console.log(`Deployed ${contractTitle}: ${address} 🚀`);
+  const data = JSON.stringify(
+    { ...deployments, [network]: contracts },
+    null,
+    2
+  );
+  fs.writeFileSync("./deployments.json", data);
+};
 
 async function main() {
   const VM = await hre.ethers.getContractFactory("VM");
   const vm = await VM.deploy();
 
-  const TESTABLE_VM = await hre.ethers.getContractFactory("TestableVM");
-  const testableVm = await TESTABLE_VM.deploy(vm.address);
-
-  console.log("Testable weiroll vm deployed to:", testableVm.address);
+  const contractName = "TestableVM";
+  if (overwrite || !contracts[contractName]) {
+    const TESTABLE_VM = await hre.ethers.getContractFactory(contractName);
+    const testableVm = await TESTABLE_VM.deploy(vm.address);
+    registerDeployment(contractName, testableVm.address);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
