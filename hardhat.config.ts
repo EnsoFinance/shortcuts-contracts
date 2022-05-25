45c72aa4ce537ab8f2c66027bfe30afe4b33f07c
Filip Malachowicz <filip.malachowicz@gmail.com>
2022-05-25 15:28:12 +0200

    Fix deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
-import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/types";
-import "hardhat-deploy";
-import "@nomiclabs/hardhat-ethers";
-import "hardhat-gas-reporter";
-import "@typechain/hardhat";
-import "solidity-coverage";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
+import * as dotenv from 'dotenv';
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+import '@typechain/hardhat';
+import 'solidity-coverage';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
 
-import { nodeUrl, accounts, addForkConfiguration } from "./utils/network";
+import {nodeUrl, accounts, addForkConfiguration} from './utils/network';
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.9",
+        version: '0.8.9',
         settings: {
           optimizer: {
             enabled: true,
@@ -35,37 +35,37 @@ const config: HardhatUserConfig = {
       initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
     },
     localhost: {
-      url: nodeUrl("localhost"),
+      url: nodeUrl('localhost'),
       accounts: accounts(),
       chainId: 31337,
     },
     production: {
-      url: nodeUrl("mainnet"),
-      accounts: accounts("mainnet"),
+      url: nodeUrl('mainnet'),
+      accounts: accounts('mainnet'),
     },
     mainnet: {
-      url: nodeUrl("mainnet"),
-      accounts: accounts("mainnet"),
+      url: nodeUrl('mainnet'),
+      accounts: accounts('mainnet'),
     },
     kovan: {
-      url: nodeUrl("kovan"),
-      accounts: accounts("kovan"),
+      url: nodeUrl('kovan'),
+      accounts: accounts('kovan'),
     },
   }),
   deterministicDeployment: {},
   paths: {
-    sources: "contracts",
+    sources: 'contracts',
   },
   gasReporter: {
-    currency: "USD",
+    currency: 'USD',
     gasPrice: 100,
     enabled: Boolean(process.env.REPORT_GAS),
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     maxMethodDiff: 10,
   },
   typechain: {
-    outDir: "typechain",
-    target: "ethers-v5",
+    outDir: 'typechain',
+    target: 'ethers-v5',
   },
   mocha: {
     timeout: 0,
@@ -78,8 +78,8 @@ const config: HardhatUserConfig = {
         deployments: {
           // process.env.HARDHAT_FORK will specify the network that the fork is made from.
           // these lines allow it to fetch the deployments from the network being forked from both for node and deploy task
-          hardhat: ["deployments/" + process.env.HARDHAT_FORK],
-          localhost: ["deployments/" + process.env.HARDHAT_FORK],
+          hardhat: ['deployments/' + process.env.HARDHAT_FORK],
+          localhost: ['deployments/' + process.env.HARDHAT_FORK],
         },
       }
     : undefined,
