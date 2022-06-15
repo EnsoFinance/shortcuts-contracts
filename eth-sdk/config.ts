5e0cd3452d511196ff749bf7b14cbbb8d28fe5b0
Filip Malachowicz <filip.malachowicz@gmail.com>
2022-06-15 11:10:52 +0200

    Fix Euler contract address

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       stETH: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
       dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
       euler: {
-        euler: '0xe5d0a7a3ad358792ba037cb6ee375ffde7ba2cd1',
+        euler: '0x27182842E098f60e3D576794A5bFFb0777E025d3',
         markets: '0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3',
         liquidation: '0xf43ce1d09050BAfd6980dD43Cde2aB9F18C85b34',
         exec: '0x59828FdF7ee634AaaD3f58B19fDBa3b03E2D9d80',
