8109fd9f896494be261107e6d5fbbc96a5ee9100
Ben Wolf <benjamineliaswolf@gmail.com>
2022-06-16 20:59:11 +0200

    Update eth-sdk config

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -9,11 +9,10 @@ export default defineConfig({
       stETH: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
       dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
       euler: {
-        euler: '0x27182842E098f60e3D576794A5bFFb0777E025d3',
-        markets: '0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3',
-        liquidation: '0xf43ce1d09050BAfd6980dD43Cde2aB9F18C85b34',
-        exec: '0x59828FdF7ee634AaaD3f58B19fDBa3b03E2D9d80',
-        swap: '0x7123C8cBBD76c5C7fCC9f7150f23179bec0bA341',
+        markets: {
+          proxy: '0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3',
+          implementation: '0xE5d0A7A3ad358792Ba037cB6eE375FfDe7Ba2Cd1',
+        },
       },
     },
   },
@@ -23,4 +22,5 @@ export default defineConfig({
   rpc: {
     mainnet: process.env.ETH_NODE_URI_MAINNET,
   },
+  outputPath: './eth-sdk/types',
 });
