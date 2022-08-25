2864833439fe04ef670cf32d1bfa6ec83acbb84a
Ben Wolf <benjamineliaswolf@gmail.com>
2022-08-25 15:16:27 +0200

    Update tests for util contracts

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -1,36 +1,23 @@
 import {expect} from './chai-setup';
 import {ethers, getNamedAccounts} from 'hardhat';
-import {Utils, Balance} from '../typechain';
-import {BigNumber, Contract} from 'ethers';
+import {BalanceV1} from '../typechain';
+import {Contract} from 'ethers';
 
 describe('Utils', function () {
-  let contracts: {[name: string]: Contract};
+  let utils: {[name: string]: Contract};
   let deployer: string;
 
   before(async () => {
     ({deployer} = await getNamedAccounts());
 
-    contracts = {
-      Utils: <Utils>await ethers.getContract('Utils'),
-      Balance: <Balance>await ethers.getContract('Balance'),
+    utils = {
+      BalanceV1: <BalanceV1>await ethers.getContract('BalanceV1'),
     };
   });
 
-  it('should return balance', async () => {
-    const encodedFunctionCall = contracts.Balance.interface.encodeFunctionData('getBalance', [deployer]);
-
-    const result = await contracts.Utils.call('Balance', encodedFunctionCall);
-    const balance = contracts.Balance.interface.decodeFunctionResult('getBalance', result);
-
-    expect(BigNumber.from(balance.toString())).to.be.gt(0);
-  });
-
-  it('should fail for non existent util', async () => {
-    const encodedFunctionCall = contracts.Balance.interface.encodeFunctionData('getBalance', [deployer]);
-    const wrongUtil = 'Lala';
-
-    await expect(contracts.Utils.call(wrongUtil, encodedFunctionCall)).to.be.revertedWith(
-      `enso: util \\"${wrongUtil}\\" does not exist`
-    );
+  describe('BalanceV1', () => {
+    it('getBalance', async () => {
+      expect(await utils.BalanceV1.getBalance(deployer)).to.eq(await ethers.provider.getBalance(deployer));
+    });
   });
 });
