fdd698a21ad66c34794fde93c66d9383de045459
Ben Wolf <benjamineliaswolf@gmail.com>
2022-09-01 02:36:51 +0200

    Add basicv1 util contract

--- contracts/Utils/BasicV1.sol
+++ contracts/Utils/BasicV1.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract BalanceV1 {
+contract BasicV1 {
     function getBalance(address balanceAddress) public view returns (uint256 balance) {
         return address(balanceAddress).balance;
+    }
+    
+    function getBlockTimestamp() public view returns (uint256 timestamp) {
+        return block.timestamp;
     }
 }
