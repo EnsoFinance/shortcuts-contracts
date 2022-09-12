a252cd7b13820a16800915d97e824ceb6f14570d
George Carder <georgercarder@gmail.com>
2022-09-12 12:44:50 -0700

    integrate clonable..upgradeable with factory, tests lacking

--- contracts/EnsoWalletFactory.sol
+++ contracts/EnsoWalletFactory.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.16;
 
 import "./EnsoWallet.sol";
 import {Clones} from "./Libraries/Clones.sol";
+import {ClonableTransparentUpgradeableProxy} from "../../contracts/helpers/ClonableTransparentUpgradeableProxy.sol";
 
 contract EnsoWalletFactory {
     using Clones for address;
 
     address public immutable ensoWallet;
+    ClonableTransparentUpgradeableProxy private _clonableTUPTemplate;
 
     event Deployed(EnsoWallet instance);
 
     constructor(address EnsoWallet_) {
         ensoWallet = EnsoWallet_;
+        _clonableTUPTemplate = new ClonableTransparentUpgradeableProxy(ensoWallet, address(this));
     }
 
-    function deploy(bytes32[] calldata commands, bytes[] calldata state) public payable returns (EnsoWallet instance) {
-        instance = EnsoWallet(payable(ensoWallet.cloneDeterministic(msg.sender)));
+    function deploy(address walletAdmin, bytes32[] calldata commands, bytes[] calldata state) public payable returns (EnsoWallet instance) {
+        instance = EnsoWallet(payable(
+          address(_clonableTUPTemplate).cloneDeterministic(msg.sender)
+        ));
+        ClonableTransparentUpgradeableProxy(payable(instance)).initialize(ensoWallet, walletAdmin);
         instance.initialize{value: msg.value}(msg.sender, commands, state);
 
         emit Deployed(instance);
