0f084290190ad5623c05e2ad1cc82a9b66250eae
Peter Phillips <github@pmphillips.ca>
2022-10-17 09:13:44 -0700

    Clonable uups proxy

--- contracts/EnsoWalletFactory.sol
+++ contracts/EnsoWalletFactory.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.16;
 
 // TODO: switch to Ownable2Step when available
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./libraries/BeaconClones.sol";
-import "./interfaces/IBeacon.sol";
+import "./libraries/UpgradeableClones.sol";
+//import "./interfaces/IBeacon.sol";
 import "./interfaces/IEnsoWallet.sol";
 
-contract EnsoWalletFactory is IBeacon, Ownable {
-    using BeaconClones for address;
+contract EnsoWalletFactory is Ownable {
+    using UpgradeableClones for address;
 
     address public ensoWallet;
 
@@ -26,14 +26,16 @@ contract EnsoWalletFactory is IBeacon, Ownable {
         bytes32[] calldata commands,
         bytes[] calldata state
     ) public payable returns (IEnsoWallet instance) {
-        instance = IEnsoWallet(payable(address(this).cloneDeterministic(msg.sender)));
+        instance = IEnsoWallet(payable(ensoWallet.cloneDeterministic(msg.sender)));
         instance.initialize{ value: msg.value }(msg.sender, commands, state);
         emit Deployed(instance);
     }
 
+    /*
     function implementation() external view override returns (address) {
         return ensoWallet;
     }
+    */
 
     // TODO: upgrade should probably be on a timelock for added security
     function upgrade(address newWallet) external onlyOwner {
@@ -48,7 +50,7 @@ contract EnsoWalletFactory is IBeacon, Ownable {
     }
 
     function getUserAddress(address user) public view returns (address payable) {
-        return payable(address(this).predictDeterministicAddress(
+        return payable(ensoWallet.predictDeterministicAddress(
             user,
             address(this)
         ));
