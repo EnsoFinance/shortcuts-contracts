1b45e07b2a746953c8a459027787636d70a0dc17
Peter Phillips <github@pmphillips.ca>
2022-10-20 08:15:18 -0700

    Basic wallet specify amounts

--- contracts/BasicWallet.sol
+++ contracts/BasicWallet.sol
@@ -15,35 +15,37 @@ contract BasicWallet {
 
     error NotOwner();
     error WithdrawFailed();
+    error InvalidArrayLength();
 
     modifier onlyOwner {
         if (msg.sender != OWNER.getAddress()) revert NotOwner();
         _;
     }
 
-    function withdrawETH() external onlyOwner {
-        (bool success, ) = msg.sender.call{ value : address(this).balance }("");
+    function withdrawETH(uint256 amount) external onlyOwner {
+        (bool success, ) = msg.sender.call{ value : amount }("");
         if (!success) revert WithdrawFailed();
     }
 
-    function withdrawERC20s(IERC20[] memory erc20s) external onlyOwner {
+    function withdrawERC20s(
+        IERC20[] memory erc20s,
+        uint256[] memory amounts
+    ) external onlyOwner {
         uint256 num = erc20s.length;
-
-        IERC20 erc20;
-        uint256 balance;
+        if (amounts.length != num) revert InvalidArrayLength();
         for (uint256 i; i < num; ) {
-            erc20 = erc20s[i];
-            balance = erc20.balanceOf(address(this));
-            if (balance > 0) erc20.safeTransfer(msg.sender, balance);
+            erc20s[i].safeTransfer(msg.sender, amounts[i]);
             unchecked {
                 ++i;
             }
         }
     }
 
-    function withdrawERC721s(IERC721 erc721, uint256[] memory ids) external onlyOwner {
+    function withdrawERC721s(
+        IERC721 erc721,
+        uint256[] memory ids
+    ) external onlyOwner {
         uint256 num = ids.length;
-
         for (uint256 i; i < num; ) {
             erc721.safeTransferFrom(address(this), msg.sender, ids[i]);
             unchecked {
@@ -52,20 +54,13 @@ contract BasicWallet {
         }
     }
 
-    function withdrawERC1155s(IERC1155 erc1155, uint256[] memory ids) external onlyOwner {
-        uint256 num = ids.length;
-
-        uint256 id;
-        uint256 balance;
-        for (uint256 i; i < num; ) {
-            id = ids[i];
-            balance = erc1155.balanceOf(address(this), id);
-            if (balance > 0)
-                erc1155.safeTransferFrom(address(this), msg.sender, id, balance, new bytes(0));
-            unchecked {
-                ++i;
-            }
-        }
+    function withdrawERC1155s(
+        IERC1155 erc1155,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) external onlyOwner {
+        // safeBatchTransferFrom will validate the array lengths
+        erc1155.safeBatchTransferFrom(address(this), msg.sender, ids, amounts, new bytes(0));
     }
 
     receive() external payable {}
