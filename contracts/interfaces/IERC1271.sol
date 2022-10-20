c8687a334a92c4f70c565983186761369f739d51
Peter Phillips <github@pmphillips.ca>
2022-10-20 15:49:09 -0700

    tab

--- contracts/interfaces/IERC1271.sol
+++ contracts/interfaces/IERC1271.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.16;
 
 interface IERC1271 {
-  function isValidSignature(
-      bytes32 _hash,
-      bytes calldata _signature
-  ) external view returns (bytes4 magicValue);
+    function isValidSignature(
+        bytes32 _hash,
+        bytes calldata _signature
+    ) external view returns (bytes4 magicValue);
 
     function isValidSignature(
        bytes calldata _message,
