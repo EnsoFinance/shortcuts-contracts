7321ec80e18b72f2054502cf22a3857b7169fe73
milonite <milonite@hotmail.it>
2022-11-16 16:47:42 +0100

    update percentage math contract, add tests

--- contracts/helpers/PercentageMathHelpers.sol
+++ contracts/helpers/PercentageMathHelpers.sol
@@ -2,23 +2,14 @@
 pragma solidity ^0.8.16;
 
 /**
- * @dev MathHelpers contract is recommended to use only in Shortcuts passed to EnsoWallet
+ * @dev PercentageMath contract is recommended to use only in Shortcuts passed to EnsoWallet
  *
- * Based on OpenZepplin Contracts v4.7.3:
- * - utils/math/Math.sol (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/Math.sol)
- * - utils/math/SafeMath.sol (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol)
+ * Forked from Aave PercentageMath library:
+ * - protocol-v2/contracts/protocol/libraries/math/PercentageMath.sol (https://github.com/aave/protocol-v2/blob/ice/mainnet-deployment-03-12-2020/contracts/protocol/libraries/math/PercentageMath.sol)
  */
 
-
-/**
- * @title PercentageMath library
- * @author Aave
- * @notice Provides functions to perform percentage calculations
- * @dev Percentages are defined by default with 2 decimals of precision (100.00). The precision is indicated by PERCENTAGE_FACTOR
- * @dev Operations are rounded half up
- **/
-
-library PercentageMath {
+contract PercentageMathHelpers {
+  uint256 public constant VERSION = 1;
   uint256 constant PERCENTAGE_FACTOR = 1e4; //percentage plus two decimals
   uint256 constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
 
@@ -28,13 +19,14 @@ library PercentageMath {
    * @param percentage The percentage of the value to be calculated
    * @return The percentage of value
    **/
-  function percentMul(uint256 value, uint256 percentage) internal pure returns (uint256) {
+
+  function percentMul(uint256 value, uint256 percentage) external pure returns (uint256) {
     if (value == 0 || percentage == 0) {
       return 0;
     }
 
     require(
-      value <= (type(uint256).max - HALF_PERCENT) / percentage,
+      value <= (type(uint256).max - HALF_PERCENT) / percentage, "multiplication overflow"
     );
 
     return (value * percentage + HALF_PERCENT) / PERCENTAGE_FACTOR;
@@ -46,13 +38,12 @@ library PercentageMath {
    * @param percentage The percentage of the value to be calculated
    * @return The value divided the percentage
    **/
-  function percentDiv(uint256 value, uint256 percentage) internal pure returns (uint256) {
-    require(percentage != 0, Errors.MATH_DIVISION_BY_ZERO);
+  function percentDiv(uint256 value, uint256 percentage) external pure returns (uint256) {
+    require(percentage != 0, "Division by 0");
     uint256 halfPercentage = percentage / 2;
 
     require(
-      value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR,
-      Errors.MATH_MULTIPLICATION_OVERFLOW
+      value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR, "multiplication overflow"
     );
 
     return (value * PERCENTAGE_FACTOR + halfPercentage) / percentage;
