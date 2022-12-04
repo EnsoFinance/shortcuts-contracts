df8b82979fd496fb301ef1bb94abededd3e66b14
Filip Malachowicz <filip.malachowicz@gmail.com>
2022-12-04 22:38:00 +0000

    Deploy PercentageMathHelpers

--- contracts/helpers/PercentageMathHelpers.sol
+++ contracts/helpers/PercentageMathHelpers.sol
@@ -9,43 +9,39 @@ pragma solidity ^0.8.16;
  */
 
 contract PercentageMathHelpers {
-  uint256 public constant VERSION = 1;
-  uint256 constant PERCENTAGE_FACTOR = 1e4; //percentage plus two decimals
-  uint256 constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
+    uint256 public constant VERSION = 1;
+    uint256 constant PERCENTAGE_FACTOR = 1e4; //percentage plus two decimals
+    uint256 constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
 
-  /**
-   * @dev Executes a percentage multiplication
-   * @param value The value of which the percentage needs to be calculated
-   * @param percentage The percentage of the value to be calculated
-   * @return The percentage of value
-   **/
+    /**
+     * @dev Executes a percentage multiplication
+     * @param value The value of which the percentage needs to be calculated
+     * @param percentage The percentage of the value to be calculated
+     * @return The percentage of value
+     **/
 
-  function percentMul(uint256 value, uint256 percentage) external pure returns (uint256) {
-    if (value == 0 || percentage == 0) {
-      return 0;
+    function percentMul(uint256 value, uint256 percentage) external pure returns (uint256) {
+        if (value == 0 || percentage == 0) {
+            return 0;
+        }
+
+        require(value <= (type(uint256).max - HALF_PERCENT) / percentage, "multiplication overflow");
+
+        return (value * percentage + HALF_PERCENT) / PERCENTAGE_FACTOR;
     }
 
-    require(
-      value <= (type(uint256).max - HALF_PERCENT) / percentage, "multiplication overflow"
-    );
+    /**
+     * @dev Executes a percentage division
+     * @param value The value of which the percentage needs to be calculated
+     * @param percentage The percentage of the value to be calculated
+     * @return The value divided the percentage
+     **/
+    function percentDiv(uint256 value, uint256 percentage) external pure returns (uint256) {
+        require(percentage != 0, "Division by 0");
+        uint256 halfPercentage = percentage / 2;
 
-    return (value * percentage + HALF_PERCENT) / PERCENTAGE_FACTOR;
-  }
+        require(value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR, "multiplication overflow");
 
-  /**
-   * @dev Executes a percentage division
-   * @param value The value of which the percentage needs to be calculated
-   * @param percentage The percentage of the value to be calculated
-   * @return The value divided the percentage
-   **/
-  function percentDiv(uint256 value, uint256 percentage) external pure returns (uint256) {
-    require(percentage != 0, "Division by 0");
-    uint256 halfPercentage = percentage / 2;
-
-    require(
-      value <= (type(uint256).max - halfPercentage) / PERCENTAGE_FACTOR, "multiplication overflow"
-    );
-
-    return (value * PERCENTAGE_FACTOR + halfPercentage) / percentage;
-  }
+        return (value * PERCENTAGE_FACTOR + halfPercentage) / percentage;
+    }
 }
