21e22e773fa37db849d951c7566aae08956a9478
Peter Phillips <github@pmphillips.ca>
2023-01-23 09:55:17 -0800

    Update license

--- contracts/interfaces/IVM.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
-
-interface IVM {
-    function execute(bytes32[] calldata commands, bytes[] calldata state) external payable returns (bytes[] memory);
-}
